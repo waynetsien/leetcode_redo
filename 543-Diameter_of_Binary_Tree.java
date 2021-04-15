4eb228be438f328d5e8226f28ee67ea97b9ebf34
waynetsien <chenhoontsien@gmail.com>
2021-04-15 19:06:44 +0800

    543. Diameter of Binary Tree

---
+++ 543-Diameter_of_Binary_Tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    // 记录目前路径最长的长度
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return diameter;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        diameter = Math.max(diameter, left + right);
+        // 返回某节点的左右两边最长的长度，并向上递归返回 +1
+        return Math.max(left, right) + 1;
+    }
+}
