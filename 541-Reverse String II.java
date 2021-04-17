e9a188d707e2c7e881ef02d81e73cd77510ec72d
waynetsien <chenhoontsien@gmail.com>
2021-04-18 02:27:57 +0800

    initial commit

--- 541-Reverse String II.java
+++ 541-Reverse String II.java
@@ -1,16 +1,16 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        char[] word = s.toCharArray();
-        int n = word.length, left = 0, right;
+        char[] words = s.toCharArray();
+        int n = words.length, left = 0, right;
 
         while (left < n) {
             // 可以控制 当前time中的字符个数num < k的情况，直接right取num
             right = Math.min(left + k - 1, n - 1);
-            reverseVal(word, left, right);
+            reverseVal(words, left, right);
             left += 2 * k;
         }
 
-        return new String(word);
+        return new String(words);
     }
 
     public void reverseVal(char[] a, int left, int right) {
