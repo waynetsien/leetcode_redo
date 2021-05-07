84835f36dae785e65d17fb26ca07dbaecadd2a96
waynetsien <chenhoontsien@gmail.com>
2021-05-07 23:37:16 +0800

    initial commit

---
+++ 21-Merge Two Sorted Lists.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // Recursion 递归的思想
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    // 迭代，遍历两个list，找出最小的放入新的list中，剩余的则直接追加到后面
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1), curNode = dummy;
+
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                curNode.next = l1;
+                l1 = l1.next;
+            } else {
+                curNode.next = l2;
+                l2 = l2.next;
+            }
+            curNode = curNode.next;
+        }
+
+        curNode.next = (l1 != null) ? l1 : l2;
+        return dummy.next;
+    }
+}
